3743429844821ac404d656f719d3d408f557323b
UserHiddenH <landoon.hugo@gmail.com>
2026-06-20 10:03:19 +0200

    Delete release directory

--- release/Desktop_Qt_6_10_2_MinGW_64_bit-Release/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Probleme_a_trois_corps_FILE C:/Users/admin/Desktop/Projet_version_final/build/Desktop_Qt_6_10_2_MinGW_64_bit-Release/Probleme_a_trois_corps.exe)
-set(__QT_DEPLOY_TARGET_Probleme_a_trois_corps_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Probleme_a_trois_corps_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6PrintSupport.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
--- release/Desktop_Qt_6_10_2_MinGW_64_bit-Release/.qt/deploy_Probleme_a_trois_corps_88cbc32e69.cmake
+++
@@ -1,8 +0,0 @@
-include("C:/Users/admin/Desktop/Projet_version_final/build/Desktop_Qt_6_10_2_MinGW_64_bit-Release/.qt/QtDeploySupport.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/Probleme_a_trois_corps-plugins.cmake" OPTIONAL)
-set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
-
-qt6_deploy_runtime_dependencies(
-    EXECUTABLE "C:/Users/admin/Desktop/Projet_version_final/build/Desktop_Qt_6_10_2_MinGW_64_bit-Release/Probleme_a_trois_corps.exe"
-    GENERATE_QT_CONF
-)
